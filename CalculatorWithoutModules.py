1b43d247a46abe929f73181badc36209f656a689
€legant $adhu <123619699+sadhubuild@users.noreply.github.com>
2023-02-01 11:48:21 +0530

    Added Calculator.py

---
+++ CalculatorWithoutModules.py
@@ -0,0 +1,42 @@
+def calc(n1,op,n2):
+
+    
+
+    if op=='+':
+
+        
+
+        return n1+n2
+
+
+
+    if op=='-':
+
+        
+
+        return n1-n2
+
+        
+
+    if op=='*':
+
+        
+
+        return n1*n2
+
+        
+
+    if op=='/':
+
+        
+
+        return n1/n2
+
+
+
+    else:
+
+        
+
+        print('Its a simple calculator.')
+
